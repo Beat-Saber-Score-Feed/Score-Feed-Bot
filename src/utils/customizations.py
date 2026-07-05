1698c6b12193f8dac1fbc22b2fc8623b71fbdb44
carterconst <constantine.carter09@gmail.com>
2026-07-05 01:51:31 -0400

    fix: replace getting enabled key with .get() so it doesnt error whenever enabled key doesnt exist

--- src/utils/customizations.py
+++ src/utils/customizations.py
@@ -47,7 +47,7 @@ DEFAULT_CUSTOMIZATIONS = {
 
 def get_customizations(channel_data):
     channel_customizations = channel_data.get("customization", {})
-    if channel_customizations["enabled"]:
+    if channel_customizations.get("enabled", False):
         customized_elements = channel_customizations.setdefault("customizations", {})
     else:
         customized_elements = {}
