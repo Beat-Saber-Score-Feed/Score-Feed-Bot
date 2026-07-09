07fc9ea14f3d7b6f5b8119cadab92a81402fa907
carterconst <constantine.carter09@gmail.com>
2026-07-09 17:03:06 -0400

    fix: fix checks for unranked maps

--- src/main.py
+++ src/main.py
@@ -82,6 +82,11 @@ async def listener():
 
                                         if not leaderboard_settings.get("enabled", True):
                                             unranked_settings = all_leaderboard_settings.get("unr", {})
+                                            if parsed_data.get("unr_pp", 0) < unranked_settings.get("pp_threshold", 0.001):
+                                                continue
+
+                                            if parsed_data.get("rank") > unranked_settings.get("rank_threshold",math.inf):
+                                                continue
                                             if unranked_settings.get("enabled", True) and "unr" not in valid_leaderboards:
                                                 valid_leaderboards.append("unr")
                                             continue
@@ -90,7 +95,12 @@ async def listener():
 
                                     if not valid_leaderboards:
                                         unranked_settings = all_leaderboard_settings.get("unr", {})
-                                        if unranked_settings.get("enabled", True):
+                                        if parsed_data.get("unr_pp", 0) < unranked_settings.get("pp_threshold", 0.001):
+                                            continue
+
+                                        if parsed_data.get("rank") > unranked_settings.get("rank_threshold", math.inf):
+                                            continue
+                                        if unranked_settings.get("enabled", True) and "unr" not in valid_leaderboards:
                                             valid_leaderboards.append("unr")
 
                                     for leaderboard in valid_leaderboards:
